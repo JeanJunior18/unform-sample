e9f62615110b507a1641d0d5a57a0affcd1992d2
Jean Junior <jclgjunior@gmail.com>
2020-03-18 13:50:06 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useRef, useEffect } from 'react';
 import './App.css';
+import { Form } from '@unform/web'
+import { Scope } from '@unform/core'
+import Input, {  } from './components/Form/Input'
+import * as Yup from 'yup'
 
 function App() {
+
+  const formRef = useRef(null)
+
+  
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      formRef.current.setData({
+        username: 'jeanjr',
+        password: 'root',
+        adress: {
+          street: 'Rua Altos',
+          number: '215',
+        },
+        education:{
+          universidade: 'Faculdade Integral Diferencial',
+          curso: 'Sistemas de Informação',
+          periodo: 2,
+        }
+      })
+    },1000)
+  },[])
+
+  async function handleSubmit(data){
+
+    try{
+      const schema = Yup.object().shape({
+      username: Yup.string().required('O nome é obrigatório'),
+      password: Yup.string().required('A senha é obrigatória'),
+      adress: Yup.object().shape({
+        street: Yup.string().required('Rua é obrigatório')
+      })
+    });
+    formRef.current.setErrors({})
+    await schema.validate(data, { abortEarly: false })
+
+    console.log(data)
+    }catch(err){
+      const ErrorMessages = {}
+      if(err instanceof Yup.ValidationError){
+        err.inner.forEach(error=>{
+          ErrorMessages[error.path] = error.message;
+        })
+      }
+        formRef.current.setErrors(ErrorMessages)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Form ref={formRef}  onSubmit={handleSubmit}>
+       <Input name="username" />
+       <Input name="password" type="password" />
+       
+       <Input name="adress.street" />
+       <Input name="adress.number" />
+
+        <Scope path="education">
+          <Input name='universidade' />
+          <Input name='curso' />
+          <Input name='periodo' />
+        </Scope>
+
+
+       <input type="submit" value="Enviar" />
+     </Form>
     </div>
   );
 }
